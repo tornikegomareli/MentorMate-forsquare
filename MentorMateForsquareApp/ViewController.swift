b73ec1cf95013bc4d1bb681608518d4e2b244e06
Tornike Gomareli <gomarelidevelopment@gmail.com>
2021-11-11 16:28:35 +0400

    Initial Commit

---
+++ MentorMateForsquareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentorMateForsquareApp
+//
+//  Created by Tornike Gomareli on 11.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
